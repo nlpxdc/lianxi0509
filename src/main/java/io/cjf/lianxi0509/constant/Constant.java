b32d52f8655dde61b1bc05ba6e4af032cf2a2e65
chenjiefei <nlpx_dc@hotmail.com>
2019-05-10 14:28:03 +0800

    add login

---
+++ src/main/java/io/cjf/lianxi0509/constant/Constant.java
@@ -0,0 +1,5 @@
+package io.cjf.lianxi0509.constant;
+
+public class Constant {
+    public static final String passwordSeperator = "$";
+}
--- src/main/java/io/cjf/lianxi0509/controller/UserController.java
+++ src/main/java/io/cjf/lianxi0509/controller/UserController.java
@@ -1,5 +1,6 @@
 package io.cjf.lianxi0509.controller;
 
+import io.cjf.lianxi0509.constant.Constant;
 import io.cjf.lianxi0509.dao.UserMapper;
 import io.cjf.lianxi0509.dto.UserCreateDTO;
 import io.cjf.lianxi0509.exception.ClientException;
@@ -7,11 +8,9 @@ import io.cjf.lianxi0509.po.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.util.DigestUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import javax.xml.bind.DatatypeConverter;
 import java.security.SecureRandom;
 
@@ -25,6 +24,9 @@ public class UserController {
 
     @Autowired
     private SecureRandom secureRandom;
+
+    @Autowired
+    private HttpSession httpSession;
 
     @PostMapping("/create")
     public Integer create(@RequestBody UserCreateDTO userCreateDTO) throws ClientException {
@@ -41,9 +43,30 @@ public class UserController {
         String password = userCreateDTO.getPassword();
         String toEncPwd = password + saltStr;
         String encPwd = DigestUtils.md5DigestAsHex(toEncPwd.getBytes());
-        String pwdToStore = String.format("%s$%s",encPwd,saltStr);
+        String passwordSeperator = Constant.passwordSeperator;
+        String pwdToStore = String.format("%s%s%s",encPwd, passwordSeperator, saltStr);
         user.setEncryptedPassword(pwdToStore);
         userMapper.insert(user);
         return user.getUserId();
+    }
+
+    @GetMapping("/login")
+    public void login(@RequestParam String username, @RequestParam String password) throws ClientException {
+        User user = userMapper.selectByUsername(username);
+        if (user == null){
+            throw new ClientException(1,"user doesn't exist");
+        }
+        String encryptedPassword = user.getEncryptedPassword();
+//        String passwordSeperator = Constant.passwordSeperator;
+        String[] encPwdSaltStr = encryptedPassword.split("\\$");
+        String encPwdOrigin = encPwdSaltStr[0];
+        String saltStr = encPwdSaltStr[1];
+        String toEncPwd = password+saltStr;
+        String encPwd = DigestUtils.md5DigestAsHex(toEncPwd.getBytes());
+        if (!encPwd.equals(encPwdOrigin)){
+            throw new ClientException(2,"password is incorrect");
+        }
+        String sessionId = httpSession.getId();
+        httpSession.setAttribute(sessionId, user);
     }
 }
